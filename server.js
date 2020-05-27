4141e45517b70acc3d67aff961fa789bff8a695a
Michał Góra <michalgorazs3@gmail.com>
2020-05-27 22:51:50 +0200

    Server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('msg');
+})
+
+app.listen(2000);
+
+
+//const http = require('http');
+
+//const server = http.createServer((request, response) => {
+//    console.log('headers', request.headers)
+//    console.log('method', request.method)
+//    console.log('url', request.url)
+
+//    const user = {
+//        name: 'Kamil',
+//        hobby: 'Programowanie'
+//    }
+
+//    response.setHeader('Content-Type', 'application/json');
+//    response.end(JSON.stringify(user));
+//})
+
+//server.listen(3000);
