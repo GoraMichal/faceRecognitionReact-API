d8990a54d590cca282ce7618b91d8aa39f03c569
Michał Góra <michalgorazs3@gmail.com>
2020-05-28 16:28:37 +0200

    postman operations

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'))
+
+//app.use((req, res, next) => {
+//    console.log('MSG');
+//    next();
+//})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+    //req.query
+    //req.body
+    //req.header
+    //req.params
     res.send('getting root');
 });
 
-//http://localhost:3000/profile
+////http://localhost:3000/profile
 app.get('/profile', (req, res) => {
     res.send('getting profile');
 });
 
-//http://localhost:3000/profile/person
-app.get('/profile/person', (req, res) => {
+////http://localhost:3000/profile
+////get, post, put, use
+app.get('/profile', (req, res) => {
     //res.send('<h2>msg</h2>');
+    console.log(req.body);
     const user = {
         name: 'Michail',
         surname: 'Tal',
     }
     res.send(user);
-})
+});
+
+app.post('/profile', (req, res) => {
+    //res.send('<h2>msg</h2>');
+    console.log(req.body);
+    res.send('Good job');
+});
 
 app.listen(3000);
 
