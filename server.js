d1e2a2a69321c537c073b253c60fdcba48f08a29
Michał Góra <michalgorazs3@gmail.com>
2020-06-04 20:59:34 +0200

    Entries /image

--- server.js
+++ server.js
@@ -98,17 +98,15 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    })
-    if (!found) {
-        res.status(404).json('User not found');
-    }
+    db('users').where('id', '=', id)
+        //.update({ entries:  })
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0])
+            //console.log(entries);
+        })
+        .catch(err => res.status(400).json('Entries error'))
 })
 
 app.listen(3000);
