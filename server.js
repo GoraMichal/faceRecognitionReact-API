8187ec9fcdcbdebd897abf0fea7a2ffd32e1cbc7
Michał Góra <michalgorazs3@gmail.com>
2020-06-04 22:21:40 +0200

    Register (transaction, hash password)

--- server.js
+++ server.js
@@ -60,17 +60,33 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
 
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date
+    var hash = bcrypt.hashSync(password);
+
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        //.then(console.log);
-        .then(user => {
-            res.json(user[0]);
-        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date
+                    })
+                    //.then(console.log);
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .then(trx.commit)
+                    .catch(trx.rollback)
+            })
+    })
+
+
         .catch(err => res.status(400).json(err));
 })
 
